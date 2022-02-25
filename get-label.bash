cd0ee1417430f273c24b8aedfe499e507d441f0b
rparikh <2824564+roopakparikh@users.noreply.github.com>
2022-02-25 23:58:27 +0000

    Adding TEAMCITY BUILD ID as the optional parameter

--- get-label.bash
+++ get-label.bash
@@ -3,7 +3,13 @@
 TAG=$(git describe --tags HEAD)
 if [[ $? -ne 0 ]]
 then
+    if [[ -z "${TEAMCITY_BUILD_ID}" ]]; then
+        SUFFIX=$(git rev-parse --short HEAD)
+    else
+        SUFFIX=${TEAMCITY_BUILD_ID}
+    fi
+
     # if we cannot get the tag, lets use the <branch>-<abbrev-sha1> as the tag name
-    TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+    TAG=$(git rev-parse --abbrev-ref HEAD)-${SUFFIX}
 fi
 echo $TAG
