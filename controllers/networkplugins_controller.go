d5c7822cd527742cc7727411b508e64b91ed7d5f
Arjun Baindur <xagent@gmail.com>
2023-02-08 15:49:44 -0800

    Update default whereabouts image to v0.4.10

--- controllers/networkplugins_controller.go
+++ controllers/networkplugins_controller.go
@@ -50,7 +50,7 @@ import (
 const (
 	DefaultNamespace        = "default"
 	MultusImage             = "docker.io/platform9/multus:v3.7.2-pmk-1"
-	WhereaboutsImage        = "docker.io/platform9/whereabouts:v0.4.9"
+	WhereaboutsImage        = "docker.io/platform9/whereabouts:v0.4.10"
 	SriovCniImage           = "docker.io/platform9/sriov-cni:v2.6.2-pmk-1"
 	SriovDpImage            = "docker.io/platform9/sriov-network-device-plugin:v3.3.2-pmk-1"
 	OvsImage                = "docker.io/platform9/openvswitch:v2.12.0"
