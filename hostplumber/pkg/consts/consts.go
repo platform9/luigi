69c5746e211efe1e73bc5c244da0390c58d60dd4
Shubham Choudhari <schoudhari@platform9.com>
2023-06-27 08:15:12 +0530

    removed old version

--- hostplumber/pkg/consts/consts.go
+++
@@ -1,9 +0,0 @@
-package consts
-
-const (
-	HostPlumberCfg     = "/etc/hostplumber/"
-	SysClassNet        = "/host/sys/class/net/"
-	SysPciDrivers      = "/host/sys/bus/pci/drivers/"
-	SysPciDevices      = "/host/sys/bus/pci/devices/"
-	RhelNetworkScripts = "/host/etc/sysconfig/network-scripts/"
-)
