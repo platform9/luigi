544448ce8824b590b87c4e20f26bd8821cdbff7b
Shubham Choudhari <schoudhari@platform9.com>
2023-07-05 11:39:41 +0530

    bug fix

--- controllers/networkplugins_webhook.go
+++ controllers/networkplugins_webhook.go
@@ -8,6 +8,8 @@ import (
 
 	nettypes "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 	plumberv1 "github.com/platform9/luigi/api/v1"
+	admissionv1 "k8s.io/api/admission/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -28,15 +30,10 @@ type NetworkPluginsValidator struct {
 func (a *NetworkPluginsValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
 
 	log := logf.FromContext(ctx)
-	networkPluginsReq := &plumberv1.NetworkPlugins{}
-	err := a.decoder.Decode(req, networkPluginsReq)
-	if err != nil {
-		log.Error(err, "Error decoding NetworkPlugins")
-		return admission.Errored(http.StatusBadRequest, err)
-	}
 
 	var networkPluginsList = &plumberv1.NetworkPluginsList{}
-	if err := a.Client.List(ctx, networkPluginsList); err != nil {
+	err := a.Client.List(ctx, networkPluginsList)
+	if err != nil && !apierrors.IsNotFound(err) {
 		log.Error(err, "Error listing NetworkPluginsList")
 		return admission.Errored(http.StatusInternalServerError, fmt.Errorf("error listing NetworkPluginsList: %w", err))
 	}
@@ -48,18 +45,44 @@ func (a *NetworkPluginsValidator) Handle(ctx context.Context, req admission.Requ
 		}
 	}
 
+	if req.Operation == admissionv1.Delete {
+		if resp, err := a.multusUninstallCheck(); err != nil {
+			log.Error(err, "error while doing checks for multus uninstall")
+			return resp
+		}
+		return admission.Allowed("Delete request")
+	}
+
+	networkPluginsReq := &plumberv1.NetworkPlugins{}
+	err = a.decoder.Decode(req, networkPluginsReq)
+	if err != nil {
+		log.Error(err, "Error decoding NetworkPlugins")
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
 	if multusExist && (networkPluginsReq.Spec.Plugins == nil || networkPluginsReq.Spec.Plugins.Multus == nil) {
-		nadExist, err := a.networkAttachmentDefinitionExists(a.Client)
-		if err != nil {
-			log.Error(err, "Error checking for NetworkAttachmentDefinition")
-			return admission.Errored(http.StatusInternalServerError, fmt.Errorf("error while fetching network attachment definition: %v", err))
+		if resp, err := a.multusUninstallCheck(); err != nil {
+			log.Error(err, "error while doing checks for multus uninstall")
+			return resp
 		}
-		if nadExist {
-			return admission.Denied("NetworkAttachmentDefinition exists on cluster. multus cannot be removed without deleting the NetworkAttachmentDefinition first")
-		}
+
 	}
 
 	return ReturnPatchedNetworkPlugins(networkPluginsReq, req)
+}
+
+func (a *NetworkPluginsValidator) multusUninstallCheck() (admission.Response, error) {
+	nadExist, err := a.networkAttachmentDefinitionExists(a.Client)
+	if err != nil {
+		err = fmt.Errorf("error while fetching network attachment definition: %v", err)
+		return admission.Errored(http.StatusInternalServerError, err), err
+	}
+	if nadExist {
+		err = fmt.Errorf("NetworkAttachmentDefinition exists on cluster. multus cannot be removed without deleting the NetworkAttachmentDefinition first")
+		return admission.Denied(err.Error()), err
+	}
+	return admission.Response{}, nil
+
 }
 
 func ReturnPatchedNetworkPlugins(networkPluginsReq *plumberv1.NetworkPlugins, req admission.Request) admission.Response {
