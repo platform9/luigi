bf361a58ff28138e7df2bcc668fc5248deb15059
Shweta chavan <shweta@platform9.com>
2024-09-18 11:25:32 +0530

    testit

--- get-label.bash
+++ get-label.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 #First set the suffix to some value, either Teamcity build-id or <abbrev-sha1>
-if [[ -z "${TEAMCITY_BUILD_ID}" ]]; then
+if [[ -z "${{ github.run_number }}" ]]; then
     SUFFIX=$(git rev-parse --short HEAD)
 else
-    SUFFIX=${TEAMCITY_BUILD_ID}
+    SUFFIX=${{ github.run_number }}
 fi
 
 # Get the tag as the version
@@ -11,8 +11,8 @@ TAG=$(git describe --tags HEAD)
 if [[ $? -ne 0 ]]
 then
     # if we cannot get the tag, lets use the <branch>-pmk-<suffix> as the tag name
-    TAG=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.]/-/g')-pmk-${SUFFIX}
+    echo "TAG=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.]/-/g')-pmk-${SUFFIX}" >> $GITHUB_ENV
 else
-    TAG=$(echo $TAG | sed 's/-.*//')-pmk-${SUFFIX}
+    echo "TAG=$(echo $TAG | sed 's/-.*//')-pmk-${SUFFIX}" >> $GITHUB_ENV
 fi
-echo $TAG
+echo ${{ env.TAG }}
